f11fbc579185bf615e9191f6716145e5e7a6bfb6
Regular Guy <Buiduk@gmail.com>
2018-09-17 01:02:28 +0700

    huhu

--- router/imgurRouter.js
+++ router/imgurRouter.js
@@ -5,39 +5,47 @@ const imgurRequest = require('request');
 
 
 imgurRouter.post("/", (req, res) => {
-    // let data = req.body;
-    let data = {
-        base64: 'fudhfdhfdf',
-        name: 'demo',
-        type: 'demo'
-    }
+    let data = req.body;
+    // let data = {
+    //     base64: 'fudhfdhfdf',
+    //     name: 'demo',
+    //     type: 'demo'
+    // }
     // console.log(req.body)
     let option = {
         url: 'https://api.imgur.com/3/image',
-        method: 'POST',
         header: {
-            'Content-Type' : "application/json",
-            'Authorization' : 'Client-ID 05b778b3d686546',
-            'Authorization' : 'Bearer 7a2cc13ed9d438518d0d21f3a29d4b407f5270eb'
+            'Client-ID': '05b778b3d686546',
+            'Bearer': '7a2cc13ed9d438518d0d21f3a29d4b407f5270eb'
         },
         body: {
-            'image' : data.base64 ,
-            'title' : data.name ,
-            'description' : '' ,
-            'name' : data.name,
-            'type' : data.type
+            'image': data.base64,
+            'title': data.name,
+            'description': '',
+            'name': data.name,
+            'type': data.type
         }
     }
+    option.json = true;
 
-    imgurRequest(option, (err, dataok) => {
-        if(err) res.status(500).send({success: 0, err})
-        else res.send({success: 1, dataok});
-    })
+    // imgurRequest(option, (err, dataok) => {
+    //     if(err) res.status(500).send({success: 0, err})
+    //     else res.send({success: 1, dataok});
+    //     })
+
+    imgurRequest.post(option, (err, dataok) => {
+        if (err) res.status(500).send({ success: 0, err })
+        else {
+            console.log(dataok.body)
+            res.send({ success: 1, dataok })
+        };
+    });
+
 
     // // console.log(data)
     // console.log(data.type)
     // console.log(data.name)
-    
+
 })
 
 module.exports = imgurRouter;
